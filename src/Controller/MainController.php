0cfe79f676fbe9adbc47d281d93b3d571eb03038
Douglas Silva <doug.hs@protonmail.ch>
2019-11-27 23:03:03 -0300

    Removed placeholder card decks and added a responsive product grid for the main page, currently displaying all products

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -24,14 +24,19 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main_page")
      */
-    public function mainPage()
+    public function mainPage(ProductRepository $repository): Response
     {
-        return $this->render('main/index.html.twig');
+        $products = $repository->findAll();
+
+        return $this->render('main/index.html.twig', [
+            'products' => $products,
+        ]);
     }
 }
