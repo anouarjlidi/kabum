f21aae05b6fcdc1d9a752d5036cae724fdf9c1c8
Douglas Silva <doug.hs@protonmail.ch>
2019-12-11 16:33:38 -0300

    Created a command to add new administrators

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright 2019 Douglas Silva (0x9fd287d56ec107ac)
+ *
+ * This file is part of KaBuM!.
+ *
+ * KaBuM! is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KaBuM!.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class CreateAdminCommand extends Command
+{
+    protected static $defaultName = 'app:create-admin';
+    private $entityManager;
+    private $passwordEncoder;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordEncoder = $passwordEncoder;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Creates an administrator account.')
+            ->addArgument('username', InputArgument::REQUIRED, 'The username of the administrator')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the administrator')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $username = $input->getArgument('username');
+        $password = $input->getArgument('password');
+
+        $user = new User;
+        $user->setUsername($username);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $output->writeln('An administrator account identified by username "' . $username . '" has been created.');
+
+        return 0;
+    }
+}
